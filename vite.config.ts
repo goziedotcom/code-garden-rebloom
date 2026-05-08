4b6abe9d7a0b3484f5f05b2b8b4bafb5abeadbeb
gozielogy <ihejigozie3@gmail.com>
2026-05-08 16:17:32 +0100

    fix: add nitro vite plugin for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart(),
+    nitro(),
     viteReact(),
     tailwindcss(),
   ],
